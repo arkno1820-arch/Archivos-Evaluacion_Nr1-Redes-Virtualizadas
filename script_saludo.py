1b71c8bc40f77a18794aabe8112339f7dd2fae96
arkno1820 <arkno1820@gmail.com>
2026-07-21 18:16:08 +0000

    Se agrega al integrante Fabian Merino

--- script_saludo.py
+++ script_saludo.py
@@ -1,2 +1,2 @@
 print("Evaluación N°1 Redes Virtualizadas")
-print("Integrantes: CesarManriquez_CesarDelgado_AlexisBaeza_JuanRetamal")
+print("Integrantes: CesarManriquez_CesarDelgado_AlexisBaeza_JuanRetamal_FabianMerino")
